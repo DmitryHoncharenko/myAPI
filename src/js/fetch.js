accfb1d4f1cfdff6ba4e456cc38b40a83ec00925
DmitryHoncharenko <67203474+DmitryHoncharenko@users.noreply.github.com>
2021-01-10 21:26:15 +0200

    comit2

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,29 +1,67 @@
+import template from "../templates/item.hbs";
+
 export default {
-  query: "moon",
+  query: "",
   page: 1,
-  perPage: 4,
+  perPage: 3,
   baseUrl: `https://api.pexels.com/v1`,
-  getFetch() {
+
+  get queryValue() {
+    return this.query;
+  },
+  set queryValue(val) {
+    return (this.query = val);
+  },
+
+  getFetch(val = this.query, z) {
     let key = "563492ad6f917000010000010bc7d6cc1a58490e999eb6b49b04379d";
-    let params = `/search?query=${this.query}`;
-      let url = this.baseUrl + params;
-      let options = {
-          method: "GET",
-          headers: {
-              Authorization: key,
-          },
-      };
-      return fetch(url, options)
-          .then((response) => {
-            //   console.log(response);
-              return response.json();
-          })
-          .then((data) => {
-              //   console.log(data);
-              return data.photos;
-          })
-          .then((result) => {
-            //   console.log(result);
+    this.queryValue = val;
+
+    // прописываем параметры запроса, согласно доков API
+    // ДОБАВЛЯЕМ ПАРАМЕТР ДЛЯ СТРАНИЦ
+    let params = `/search?query=${this.query}&per_page=${this.perPage}&page=${this.page}`;
+
+    // сливаем встроку запроса перед отправкой
+    let url = this.baseUrl + params;
+    // создаем объект опций для запроса, по докам API, для передачи ключа
+    let options = {
+      method: "GET",
+      headers: {
+        Authorization: key,
+      },
+    };
+
+    // собственно запрос и обработка ответа на него
+    return fetch(url, options)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        return data.photos;
+      })
+      .then((result) => {
+        const items = template(result);
+        z.insertAdjacentHTML("beforeend", items);
+        setTimeout(() => {
+          window.scrollTo({
+            top: z.scrollHeight,
+            behavior: "smooth",
           });
+        }, 0);
+        return z;
+      });
+  },
+
+  // метод добавления страницы
+  setPage() {
+    this.page += 1;
+    console.log("page: ", this.page);
+    return this.page;
+  },
+  // метод сброса страниц
+  resetPage() {
+    this.page = 1;
+    console.log("reset page", this.page);
+    return this.page;
   },
 };
